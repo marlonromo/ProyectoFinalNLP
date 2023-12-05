eab21127a348fad83b2a44eb0d0dc84b3a740814
Marlon Romo <57421049+marlonromo@users.noreply.github.com>
2023-12-04 21:02:57 -0600

    Transcriptor m4a, mp3, wav

--- VoiceText.py
+++ VoiceText.py
@@ -1,34 +1,38 @@
-import streamlit as st
-import whisper
-from transformers import pipeline
+import streamlit as st # Creación de aplicaciones web interactivas.
+import whisper # Cargar y utilizar el modelo de reconocimiento de voz.
+from transformers import pipeline # cargar el modelo de resumen de Hugging Face.
 
 # Marlon Romo
-# Cargar el modelo de resumen de Hugging Face
+#  Inicializa un pipeline para el modelo de resumen de Hugging Face.
 summarizer = pipeline("summarization")
 
 st.title("Whisper App")
 
-audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"]) # Crea un componente de carga de archivos para archivos de audio en formatos específicos.
 
-model = whisper.load_model("base")
+model = whisper.load_model("base") # Carga el modelo de reconocimiento de voz Whisper.
 st.text("Whisper model loaded")
 
+# Crea un botón en la barra lateral que inicia el proceso de transcripción cuando se presiona.
 if st.sidebar.button("Transcribe Audio"):
     if audio_file is not None:
         st.sidebar.success("Transcribing Audio")
-        transcription = model.transcribe(audio_file.name)
+        # Realiza la transcripción del archivo de audio cargado.
+        transcription = model.transcribe(audio_file.name) 
         st.sidebar.success("Transcription Complete")
         st.markdown("### Transcription")
+        # Muestra el texto transcribido en la interfaz principal.
         st.markdown(transcription["text"])
         # Obtener el texto transcribido
         transcribed_text = transcription["text"]
         # Generar un resumen del texto transcribido
         summary = summarizer(transcribed_text, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)
-        # Mostrar el resumen
+        # Muestra el resumen generado en la interfaz.
         st.markdown("### Transcription Summary")
         st.markdown(summary[0]['summary_text'])
     else:
         st.sidebar.error("Please upload an audio file")
 
 st.sidebar.header("Play Original Audio File")
+# Agrega un componente para reproducir el archivo de audio original en la barra lateral.
 st.sidebar.audio(audio_file)
